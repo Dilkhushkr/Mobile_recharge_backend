60a7bcbf0882b46db4f8e2df88d12d7f31fbd63b
Dilkhushkr <128610575+Dilkhushkr@users.noreply.github.com>
2025-11-22 22:24:26 +0530

    last_change

--- src/app.ts
+++ src/app.ts
@@ -7,7 +7,10 @@ import cors from 'cors'
 const app = express();
 app.use(cors({
 
-  origin: 'http://localhost:5173', 
+  origin: [
+    "http://localhost:5173",
+    "https://adityacar1.netlify.app"
+  ],
   methods: ['GET', 'POST', 'PUT', 'DELETE'],
   credentials: true,
   allowedHeaders: ["Content-Type", "Authorization"],
